86dbca1e03050cb0a0862f537a7f934fe55f80c1
Stepan Dulka <fotosteuge@gmail.com>
2022-01-11 15:15:51 +0200

    fix problems

--- src/com/cursor/Main.java
+++ src/com/cursor/Main.java
@@ -2,49 +2,103 @@ package com.cursor;
 
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class Main {
 
-    public static void main(String[] args) throws WrongPasswordException,WrongLoginException {
-        String login = "Please, enter your login:";
-        String password = "Please, enter your password: ";
-        String confirmPassword = "Confirm Password: ";
+    private static String passwordResult;
+    private static String confirmPasswordResult;
 
+    public static void main(String[] args) throws WrongPasswordException, WrongLoginException {
+
+        while (!login(initScanner("Please, enter your Login"))) {
+            login(initScanner("Please, enter your Login"));
+            break;
+        }
+        while (!password(initScanner("Please, enter your Password"))) {
+            password(initScanner("Please, enter your Password"));
+            break;
+        }
+        while (!confirmPassword(initScanner("Please, confirm Password"))) {
+            confirmPassword(initScanner("Please, confirm Password"));
+
+        }
+        passwordEquals(passwordResult, confirmPasswordResult);
+    }
+
+    public static String initScanner(String inputType) {
         Scanner scanner = new Scanner(System.in);
-
-            System.out.println(login);
-            String inLog = scanner.nextLine();
-            checkInput(login,password,confirmPassword);
-            System.out.println(password);
-            String inPass = scanner.nextLine();
-            checkInput(login,password,confirmPassword);
-            System.out.println(confirmPassword);
-            String inConfPass = scanner.nextLine();
-            checkInput(login,password,confirmPassword);
-
-        }
+        System.out.println(inputType);
+        inputType = scanner.nextLine();
+        return inputType;
+    }
 
 
+    public static boolean checkInputCorrect(String input) {
+        Pattern pattern = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[_])(?=\\S+$).{1,20}$");
+        Matcher matcher = pattern.matcher(input);
+        return matcher.matches();
+    }
 
-    private static boolean checkInput(String login, String password, String confirmPassword)
-            throws WrongLoginException, WrongPasswordException {
 
-        if (!login.matches("[\\w]{1,20}")) {
+    public static boolean login(String login) {
+        boolean flag = false;
+        if (checkInputCorrect(login)) {
+            flag = true;
+            System.out.println("Login is correct");
+        } else {
+            flag = false;
             try {
-                throw new WrongLoginException("Incorrect login!");
-            } catch (WrongLoginException e) {
-                System.out.println("You have entered incorrect login! Please, try again!");
-            }
-        } else if (!password.matches("[\\w]{1,20}") ||
-                !confirmPassword.matches("[\\w]{1,20}") ||
-                !password.equals(confirmPassword)) {
-            try {
-                throw new WrongPasswordException("Incorrect password!");
-            } catch (WrongPasswordException e) {
-                System.out.println("You have entered incorrect password! Please, try again!");
+                throw new WrongLoginException("Login is WRONG!");
+            } catch (WrongLoginException exception) {
+                System.out.println(exception.getMessage());
             }
         }
-        return false;
+        return flag;
+    }
+
+    public static boolean password(String password) {
+        boolean flag = false;
+        if (checkInputCorrect(password)) {
+            flag = true;
+            System.out.println("Password is correct");
+            passwordResult = password;
+        } else {
+            flag = false;
+            try {
+                throw new WrongPasswordException("Password is WRONG!");
+            } catch (WrongPasswordException exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
+        return flag;
+    }
+
+    public static boolean confirmPassword(String confirmPassword) {
+        boolean flag = false;
+        if (checkInputCorrect(confirmPassword)) {
+            flag = true;
+            System.out.println("Password is correct");
+            confirmPasswordResult = confirmPassword;
+        } else {
+            flag = false;
+            try {
+                throw new WrongPasswordException("Password is WRONG!");
+            } catch (WrongPasswordException exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
+        return flag;
+    }
+
+    private static void passwordEquals(String passwordResult, String confirmPasswordResult) {
+        if (passwordResult.equals(confirmPasswordResult)) {
+            System.out.println("Passwords is Equals");
+        } else {
+            System.out.println("Passwords not Equals");
+        }
     }
 }
+   
